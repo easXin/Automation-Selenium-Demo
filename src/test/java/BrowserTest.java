d222c79626a4c6ec52dbe5de73cfc67d1eb71f24
easXin <codingericwang@gmail.com>
2020-03-10 02:25:56 -0700

    learn how to find web element and locate element via xPath

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -1,14 +1,32 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest {
 	
 	public static void main(String[] args) {
 		String projectPath=System.getProperty("user.dir");
-		System.out.println("projectPath: "+projectPath);
 		System.setProperty("webdriver.chrome.driver",projectPath+"\\driver\\googleDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://mvnrepository.com");
+		driver.get("https://google.com/");
 		
+		//WebElement textBox = driver.findElement(By.className("gLFyf"));
+		WebElement textBox = driver.findElement(By.xpath("//input[@name='q']"));
+		textBox.sendKeys("Automation Step by Step");
+		List<WebElement>listOfInputELements = driver.findElements(By.xpath("//input"));
+		
+		int count = listOfInputELements.size();
+		
+		System.out.println("Count of input elements : " + count);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.close();
 	}
 }
